0d99f38de8711c8c740e9f5b0ad8a34e687203b4
Admin <admin@admins-mbp.fritz.box>
2018-12-12 19:57:13 +0100

    alles kopiert und funktioniert

--- PewDiePie vs T-Series/PewDiePie vs T-Series/ViewController.swift
+++ PewDiePie vs T-Series/PewDiePie vs T-Series/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension String
+{
+    func counter() -> String
+    {
+        if let regex = try? NSRegularExpression(pattern: "[0-9,]+ subscribers", options: .caseInsensitive)
+        {
+            let string = self as NSString
+            
+            return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length:string.length)).map {
+                string.substring(with: $0.range).replacingOccurrences(of: " subscribers", with: "").lowercased()} ?? ""
+        }
+        
+        return self
     }
-
-
 }
 
+class ViewController: UIViewController {
+    @IBOutlet weak var pewdiesubs: UILabel!
+    @IBOutlet weak var tseriessubs: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pewsubs()
+        tsubs()
+        
+        _ = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(pewsubs), userInfo: nil, repeats: true)
+        _ = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(tsubs), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func pewsubs() {
+        let myURLString = "https://www.youtube.com/user/PewDiePie"
+        guard let myURL = URL(string: myURLString) else {
+            pewdiesubs.text = "Error: \(myURLString) doesn't seem to be a valid URL"
+            return
+        }
+        
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            pewdiesubs.text = "\(myHTMLString.counter())"
+        } catch let error {
+            pewdiesubs.text = "URL: \(myURL) -- Error: \(error)"
+        }
+        
+    }
+    
+    
+    @objc func tsubs() {
+        let myURLString = "https://www.youtube.com/user/tseries"
+        guard let myURL = URL(string: myURLString) else {
+            tseriessubs.text = "Error: \(myURLString) doesn't seem to be a valid URL"
+            return
+        }
+        
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            tseriessubs.text = "\(myHTMLString.counter())"
+        } catch let error {
+            tseriessubs.text = "URL: \(myURL) -- Error: \(error)"
+        }
+        
+    }
+}
